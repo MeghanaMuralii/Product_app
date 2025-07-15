c436d3748c6dc5f1ba93331db4bdcdfe768a4021
MeghanaMurali <meghanaminnu1234@gmail.com>
2025-07-15 23:08:27 +0530

    completed

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -9,7 +9,7 @@ const AddProduct = () => {
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Product Name</label>
                             <input type="text" className="form-control" />
                         </div>
@@ -17,31 +17,31 @@ const AddProduct = () => {
                             <label htmlFor="" className="form-label">Description</label>
                             <textarea name="" id="" className="form-control"></textarea>
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Warranty</label>
                             <input type="text" className="form-control" />
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Expiry Date</label>
                             <input type="date" name="" id="" className="form-control" />
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Manufacturing Date</label>
                             <input type="date" name="" id="" className="form-control" />
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Price</label>
                             <input type="text" className="form-control" />
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Product Photo</label>
                             <input type="file" name="" id="" className="form-control" />
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Customer CareNo</label>
-                            <input type="text" className="flow-control" />
+                            <input type="text" className="form-control" />
                         </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <button className="btn btn-success">SUBMIT</button>
                         </div>
                     </div>
--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavProduct from './NavProduct'
 
 const ViewProduct = () => {
+    const [product,changeProduct]=useState(
+        [
+            
+                {"name":"Cetaphile", "image":"https://cdn.tirabeauty.com/v2/billowing-snowflake-434234/tira-p/wrkr/products/pictures/item/free/resize-w:1080/WwvMh0iaq-Cetaphil-Moisturising-Lotion-(100ml).jpeg"},
+                {"name":"Face serum", "image":"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ8kVhaY2S8NWjPHTgoAtO1VWQlNYDvSV1LyvPFTmQn0To9lxd_1yn9et9FJLAxt7k-rAfJzDjYTudo9hQmKqBMeIDG8krR87JdBEKO0oIexXxxEKkAS-WWnh7cVybdxHZ4dhrneA&usqp=CAc"},
+                {"name":"Primer", "image":"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7fgFerBHxxr9cJH2q_y3BFnDU05c0XFbzlGRExS_63ilfaD47BAUqSXw5DLiZj0ueROYeSHkHb8vQBtWKd8DWhDWOp6oaGJ6rWen9RDd5F7_1fzPiZwBvfDFQT_x_kVjedvk2kQ&usqp=CAc"},
+                {"name":"Foundation", "image":"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSeHaOnZy43RlXdQO9zRtr0QwqbIyx0RQczNxctjCByF5H4yRy7ehvVgYIJZJRs2YEiAby91y4PD-cbgNZSgZ2ncWRkglC8Afkeg5eoIMeH4_P7wMx26H8Fd3Dn-lHVmg&usqp=CAc"},
+                {"name":"Mascara", "image":"https://cdn.tirabeauty.com/v2/billowing-snowflake-434234/tira-p/wrkr/products/pictures/item/free/resize-w:1080/1154802/MhM639n1g-1154802_1.jpg"},
+                {"name":"Blush", "image":"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxAruz_544lwH78QbMJXLSEg-w9W6kYUO2O-_aDL5kZBflLSA1OPV4JeV8uTeYkWw3b7HdSn-A3TR3P7kNOtuAkxZRGfPv012riCdq2l_DLxB72kHN8gu2QaTtoBgfCdcdjccY-w&usqp=CAc"},
+                {"name":"Cetaphile", "image":"https://cdn.tirabeauty.com/v2/billowing-snowflake-434234/tira-p/wrkr/products/pictures/item/free/resize-w:1080/WwvMh0iaq-Cetaphil-Moisturising-Lotion-(100ml).jpeg"},
+                {"name":"Face serum", "image":"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ8kVhaY2S8NWjPHTgoAtO1VWQlNYDvSV1LyvPFTmQn0To9lxd_1yn9et9FJLAxt7k-rAfJzDjYTudo9hQmKqBMeIDG8krR87JdBEKO0oIexXxxEKkAS-WWnh7cVybdxHZ4dhrneA&usqp=CAc"},
+                {"name":"Primer", "image":"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7fgFerBHxxr9cJH2q_y3BFnDU05c0XFbzlGRExS_63ilfaD47BAUqSXw5DLiZj0ueROYeSHkHb8vQBtWKd8DWhDWOp6oaGJ6rWen9RDd5F7_1fzPiZwBvfDFQT_x_kVjedvk2kQ&usqp=CAc"},
+                {"name":"Foundation", "image":"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSeHaOnZy43RlXdQO9zRtr0QwqbIyx0RQczNxctjCByF5H4yRy7ehvVgYIJZJRs2YEiAby91y4PD-cbgNZSgZ2ncWRkglC8Afkeg5eoIMeH4_P7wMx26H8Fd3Dn-lHVmg&usqp=CAc"},
+                {"name":"Mascara", "image":"https://cdn.tirabeauty.com/v2/billowing-snowflake-434234/tira-p/wrkr/products/pictures/item/free/resize-w:1080/1154802/MhM639n1g-1154802_1.jpg"},
+                {"name":"Blush", "image":"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxAruz_544lwH78QbMJXLSEg-w9W6kYUO2O-_aDL5kZBflLSA1OPV4JeV8uTeYkWw3b7HdSn-A3TR3P7kNOtuAkxZRGfPv012riCdq2l_DLxB72kHN8gu2QaTtoBgfCdcdjccY-w&usqp=CAc"}
+                
+
+        ]
+    )
+    
     return (
         <div>
             <NavProduct />
@@ -9,106 +29,28 @@ const ViewProduct = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            { product.map(
+                                (value,index)=>{
+                                    return (<div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
                                 <div class="card" >
-                                    <img src="https://cdn.tirabeauty.com/v2/billowing-snowflake-434234/tira-p/wrkr/products/pictures/item/free/resize-w:1080/WwvMh0iaq-Cetaphil-Moisturising-Lotion-(100ml).jpeg" class="card-img-top" alt="..." />
+                                    <img src={value.image} class="card-img-top" alt="..." />
                                     <div class="card-body">
-                                        <h5 class="card-title">Cetaphile</h5>
+                                        <h5 class="card-title">{value.name}</h5>
                                         <p class="card-text">Rs.350</p>
                                         <a href="#" class="btn btn-primary">BUY NOW</a>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ8kVhaY2S8NWjPHTgoAtO1VWQlNYDvSV1LyvPFTmQn0To9lxd_1yn9et9FJLAxt7k-rAfJzDjYTudo9hQmKqBMeIDG8krR87JdBEKO0oIexXxxEKkAS-WWnh7cVybdxHZ4dhrneA&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Face Serum</h5>
-                                        <p class="card-text">Rs.2050</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7fgFerBHxxr9cJH2q_y3BFnDU05c0XFbzlGRExS_63ilfaD47BAUqSXw5DLiZj0ueROYeSHkHb8vQBtWKd8DWhDWOp6oaGJ6rWen9RDd5F7_1fzPiZwBvfDFQT_x_kVjedvk2kQ&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Primer</h5>
-                                        <p class="card-text">Rs.2500</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSeHaOnZy43RlXdQO9zRtr0QwqbIyx0RQczNxctjCByF5H4yRy7ehvVgYIJZJRs2YEiAby91y4PD-cbgNZSgZ2ncWRkglC8Afkeg5eoIMeH4_P7wMx26H8Fd3Dn-lHVmg&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Foundation</h5>
-                                        <p class="card-text">Rs.1000</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://cdn.tirabeauty.com/v2/billowing-snowflake-434234/tira-p/wrkr/products/pictures/item/free/resize-w:1080/1154802/MhM639n1g-1154802_1.jpg" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Mascara</h5>
-                                        <p class="card-text">Rs.500</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSVypo_5rOp0dORlO_G89FUieM_RntV3v4d0CTpXAqH45mktOSkpmpCCaP-DdR1lwr5Ygyn78S11Ipb5sucfvb-wm-LMEC-Bx1zHAg2YuXHatP42N4wQhPY1Os5pt8M6IyCol7sIxk&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Lipstick</h5>
-                                        <p class="card-text">Rs.3500</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTWkWcd7jo1nPaIVyUG5yXY5GuaQOs7I38AYd8_lJALHUJJdEBGKMGTwuMmo653ZOm84iu5n-mOWAFNTU3FH3w0LABOSVN9xwxdHOO1r07EQtgVltIs7Bt1AWmre9N_6fw5OmcNu58&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Day Cream</h5>
-                                        <p class="card-text">Rs.999</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxjXOoajvANmTv3er2pDFlIdvt1ngw2YCqeCHtjwgbwS9L8dbHoLXNT1tNwjxtKoCM6UnI26LDGYtN9AM12WdGumK1YVOqoCBZYrrTumU&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Face Mask</h5>
-                                        <p class="card-text">Rs.1500</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQopb0eOxW42Xl8S-gr17gA11HoGcQX1V-SDmWbgbJFZ1Vtf54zXKo3fkDaB_QdCsBADsdpelwlcqKmi5Kpw3eLlrO7oDSN3D3ZLL6InE5RU_Oe3amCSHU-1rE2_AHr12R8GqDBX30&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Cleanser</h5>
-                                        <p class="card-text">Rs.350</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxAruz_544lwH78QbMJXLSEg-w9W6kYUO2O-_aDL5kZBflLSA1OPV4JeV8uTeYkWw3b7HdSn-A3TR3P7kNOtuAkxZRGfPv012riCdq2l_DLxB72kHN8gu2QaTtoBgfCdcdjccY-w&usqp=CAc" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Blush </h5>
-                                        <p class="card-text">Rs.990</p>
-                                        <a href="#" class="btn btn-primary">BUY NOW</a>
-                                    </div>
-                                </div>
-                            </div>
+                            </div>)
+                                }
+                            )}
+                            
+                            
+                            
+                            
+                            
+                            
+                           
+                            
                         </div>
                     </div>
                 </div>
